d925be6c6e3e2a310ba452dbed879d32c1480282
CRUEL Project ™ <mohamadkhoshnava@users.noreply.github.com>
2016-12-29 10:47:44 +0330

    BY CRUEL TEAM
    BY CRUEL TEAM

---
+++ bot/config.lua
@@ -0,0 +1,20 @@
+do local _ = {
+  enabled_plugins = {
+    "gm",
+"locks"
+  },
+  group  = {
+    data = "bot/group.json"
+  },
+  sudo_users = {
+    268021336,
+    260446917,
+    223404066
+  },
+robot = {
+    306386341,
+    0
+  }
+}
+return _
+end
