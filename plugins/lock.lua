6a9efe8f5555425dec1ce5d0297eed855c7cec0a
CRUEL Project ™ <CRUELTM@users.noreply.github.com>
2016-12-30 09:35:41 +0330

    BY CRUEL TEAM

---
+++ plugins/lock.lua
@@ -0,0 +1,431 @@
+local function settings(msg, target,group)
+local group = load_data('bot/group.json')
+pm = '🔧SuperGroup settings 🔧'
+pm = pm..'\n ⚙<b>Lock Links</b> : '..group[tostring(target)]['settings']['lock_link']..'⚙'
+pm = pm..'\n ⚙<b>Lock Username</b> : '..(group[tostring(target)]['settings']['lock_username'] or 'no')..'⚙'
+pm = pm..'\n ⚙<b>Lock Edit</b> : '..group[tostring(target)]['settings']['lock_edit']..'⚙'
+pm = pm..'\n ⚙<b>Lock Fwd</b> : '..(group[tostring(target)]['settings']['lock_fwd'] or 'no')..'⚙'
+pm = pm..'\n ⚙<b>Lock Spam</b> : '..(group[tostring(target)]['settings']['lock_spam'] or 'no')..'⚙'
+pm = pm..'\n     🔧<b>Mute</b>🔧'
+pm = pm..'\n ⚙<b>Mute All</b> : '..(group[tostring(target)]['settings']['mute_all'] or 'no')..'⚙'
+pm = pm..'\n ⚙<b>Mute Photo</b> : '..(group[tostring(target)]['settings']['mute_photo'] or 'no')..'⚙'
+pm = pm..'\n ⚙<b>Mute Video</b> : '..(group[tostring(target)]['settings']['mute_video'] or 'no')..'⚙'
+pm = pm..'\n ⚙<b>Mute Voice</b> : '..(group[tostring(target)]['settings']['mute_voice'] or 'no')..'⚙'
+pm = pm..'\n ⚙<b>Mute Document</b> : '..(group[tostring(target)]['settings']['mute_document'] or 'no')..'⚙'
+pm = pm..'\n ⚙<b>Mute Audio</b> : '..(group[tostring(target)]['settings']['mute_audio'] or 'no')..'⚙'
+pm = pm..'\n ⚙<b>Mute Gif</b> : '..(group[tostring(target)]['settings']['mute_gif'] or 'no')..'⚙'
+pm = pm..'\n<b>  create by cruel</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+end
+local function lock_group_links(msg, target)
+local group = load_data('bot/group.json')
+  local group_link_lock = group[tostring(target)]['settings']['lock_link']
+  if group_link_lock == 'yes' then
+    pm = 'قفل لینک فعال بود.'
+  tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+else
+    group[tostring(target)]['settings']['lock_link'] = 'yes'
+    save_data(_config.group.data, group)
+    pm = 'قفل لینک فعال شد.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function lock_group_username(msg, target)
+local group = load_data('bot/group.json')
+  local group_username_lock = group[tostring(target)]['settings']['lock_username']
+  if group_username_lock == 'yes' then
+    pm = 'قفل ارسال نام کاربری فعال بود.'
+  tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+else
+    group[tostring(target)]['settings']['lock_username'] = 'yes'
+    save_data(_config.group.data, group)
+    pm = 'قفل ارسال نام کاربری فعال شد.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function lock_group_edit(msg, target)
+local group = load_data('bot/group.json')
+  local group_edit_lock = group[tostring(target)]['settings']['lock_edit']
+  if group_edit_lock == 'yes' then
+    pm = 'قفل ویرایش پیام فعال بود.'
+  tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+else
+    group[tostring(target)]['settings']['lock_edit'] = 'yes'
+    save_data(_config.group.data, group)
+    pm = 'قفل ویرایش پیام فعال شد.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function lock_group_fwd(msg, target)
+local group = load_data('bot/group.json')
+  local group_fwd_lock = group[tostring(target)]['settings']['lock_fwd']
+  if group_fwd_lock == 'yes' then
+    pm = 'قفل فوروارد پیام فعال بود.'
+  tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+else
+    group[tostring(target)]['settings']['lock_fwd'] = 'yes'
+    save_data(_config.group.data, group)
+    pm = 'قفل فوروارد پیام فعال شد.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+
+
+local function unlock_group_links(msg, target)
+local group = load_data('bot/group.json')
+  local group_link_lock = group[tostring(target)]['settings']['lock_link']
+  if group_link_lock == 'no' then
+    pm = 'قفل لینک غیر فعال بوده است.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['lock_link'] = 'no'
+    save_data(_config.group.data, group)
+    pm= 'قفل لینک غیر فعال شد.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function unlock_group_username(msg, target)
+local group = load_data('bot/group.json')
+  local group_username_lock = group[tostring(target)]['settings']['lock_username']
+  if group_username_lock == 'no' then
+    pm = 'قفل ارسال نام کاربری غیر فعال بوده است.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['lock_username'] = 'no'
+    save_data(_config.group.data, group)
+    pm= 'قفل ارسال نام کربری غیر فعال شد.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function unlock_group_edit(msg, target)
+local group = load_data('bot/group.json')
+  local group_edit_lock = group[tostring(target)]['settings']['lock_edit']
+  if group_edit_lock == 'no' then
+    pm = 'قفل ویرایش پیام غیر فعال بوده است.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['lock_edit'] = 'no'
+    save_data(_config.group.data, group)
+    pm= 'قفل ویرایش پیام غیر فعال شد.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function unlock_group_fwd(msg, target)
+local group = load_data('bot/group.json')
+  local group_fwd_lock = group[tostring(target)]['settings']['lock_fwd']
+  if group_fwd_lock == 'no' then
+    pm = 'قفل فوروارد غیر فعال بوده است.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['lock_fwd'] = 'no'
+    save_data(_config.group.data, group)
+    pm= 'قفل فوروارد غیر فعال شد.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function lock_group_spam(msg, target)
+local group = load_data('bot/group.json')
+  local group_spam_lock = group[tostring(target)]['settings']['lock_spam']
+  if group_spam_lock == 'yes' then
+    pm = 'قفل اسپم غیر فعال بوده است.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['lock_spam'] = 'yes'
+    save_data(_config.group.data, group)
+    pm= 'قفل اسپم غیر فعال شد.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function unlock_group_spam(msg, target)
+local group = load_data('bot/group.json')
+  local group_spamlock = group[tostring(target)]['settings']['lock_spam']
+  if group_spam_lock == 'no' then
+    pm = 'قفل اسپم غیر فعال بوده است.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['lock_spam'] = 'no'
+    save_data(_config.group.data, group)
+    pm= 'قفل اسپم غیر فعال شد.'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function mute_all_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_all = group[tostring(target)]['settings']['mute_all']
+  if mute_all  == 'yes' then
+    pm = '<b>Mute All:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_all'] = 'yes'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute All:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function unmute_all_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_all = group[tostring(target)]['settings']['mute_all']
+  if mute_all  == 'no' then
+    pm = '<b>Mute All:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_all'] = 'no'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute All:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function mute_photo_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_all = group[tostring(target)]['settings']['mute_photo']
+  if mute_all  == 'yes' then
+    pm = '<b>Mute Photo:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_photo'] = 'yes'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Photo:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function unmute_photo_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_all = group[tostring(target)]['settings']['mute_photo']
+  if mute_all  == 'no' then
+    pm = '<b>Mute Photo:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_photo'] = 'no'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Photo:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function mute_video_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_video = group[tostring(target)]['settings']['mute_video']
+  if mute_video  == 'yes' then
+    pm = '<b>Mute Video:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_video'] = 'yes'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Video:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function unmute_video_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_video = group[tostring(target)]['settings']['mute_video']
+  if mute_video  == 'no' then
+    pm = '<b>Mute Video:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_video'] = 'no'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Video:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function mute_gif_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_gif = group[tostring(target)]['settings']['mute_gif']
+  if mute_gif  == 'yes' then
+    pm = '<b>Mute Gif:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_gif'] = 'yes'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Gif:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function unmute_gif_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_gif = group[tostring(target)]['settings']['mute_gif']
+  if mute_gif  == 'no' then
+    pm = '<b>Mute Gif:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_gif'] = 'no'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Gif:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function mute_voice_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_voice = group[tostring(target)]['settings']['mute_voice']
+  if mute_voice  == 'yes' then
+    pm = '<b>Mute Voice:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_voice'] = 'yes'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Voice:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function unmute_voice_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_voice = group[tostring(target)]['settings']['mute_voice']
+  if mute_voice  == 'no' then
+    pm = '<b>Mute Voice:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_voice'] = 'no'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Voice:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function mute_audio_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_audio = group[tostring(target)]['settings']['mute_audio']
+  if mute_audio  == 'yes' then
+    pm = '<b>Mute Audio:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_audio'] = 'yes'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Audio:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function unmute_audio_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_audio = group[tostring(target)]['settings']['mute_audio']
+  if mute_audio  == 'no' then
+    pm = '<b>Mute Audio:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_audio'] = 'no'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Audio:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function mute_document_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_document = group[tostring(target)]['settings']['mute_document']
+  if mute_document  == 'yes' then
+    pm = '<b>Mute Document:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_document'] = 'yes'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Document:yes</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function unmute_document_group(msg, target)
+local group = load_data('bot/group.json')
+  local mute_document = group[tostring(target)]['settings']['mute_document']
+  if mute_document  == 'no' then
+    pm = '<b>Mute Document:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  else
+    group[tostring(target)]['settings']['mute_document'] = 'no'
+    save_data(_config.group.data, group)
+    pm= '<b>Mute Document:no</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+  end
+end
+local function group_settings(msg, target)
+local group = load_data('bot/group.json')
+pm = '🔧SuperGroup settings 🔧'
+pm = pm..'\n ⚙<b>Lock Links</b> : '..group[tostring(target)]['settings']['lock_link']..'⚙'
+pm = pm..'\n ⚙<b>Lock Username</b> : '..group[tostring(target)]['settings']['lock_username']..'⚙'
+pm = pm..'\n ⚙<b>Lock Edit</b> : '..group[tostring(target)]['settings']['lock_edit']..'⚙'
+pm = pm..'\n ⚙<b>Lock Fwd</b> : '..group[tostring(target)]['settings']['lock_fwd']..'⚙'
+pm = pm..'\n ⚙<b>Lock Spam</b> : '..group[tostring(target)]['settings']['lock_spam']..'⚙'
+pm = pm..'\n     🔧<b>Mute</b>🔧'
+pm = pm..'\n ⚙<b>Mute All</b> : '..group[tostring(target)]['settings']['mute_all']..'⚙'
+pm = pm..'\n ⚙<b>Mute Photo</b> : '..group[tostring(target)]['settings']['mute_photo']..'⚙'
+pm = pm..'\n ⚙<b>Mute Video</b> : '..group[tostring(target)]['settings']['mute_video']..'⚙'
+pm = pm..'\n ⚙<b>Mute Voice</b> : '..group[tostring(target)]['settings']['mute_voice']..'⚙'
+pm = pm..'\n ⚙<b>Mute Document</b> : '..group[tostring(target)]['settings']['mute_document']..'⚙'
+pm = pm..'\n ⚙<b>Mute Audio</b> : '..group[tostring(target)]['settings']['mute_audio']..'⚙'
+pm = pm..'\n ⚙<b>Mute Gif</b> : '..group[tostring(target)]['settings']['mute_gif']..'⚙'
+pm = pm..'\n<b>  create by cruel</b>'
+tg.sendMessage(msg.chat_id_, 0, 1, pm, 1, 'html')
+end
+local function run(msg, matches)
+local addgroup = group[tostring(msg.chat_id)]
+if addgroup and is_momod(msg) then
+if matches[1] == 'settings'  then
+group_settings(msg, msg.chat_id)
+elseif matches[1] == 'lock' then
+if matches[2] == 'links' then
+lock_group_links(msg, msg.chat_id)
+elseif matches[2] == 'edit' then
+lock_group_edit(msg, msg.chat_id)
+elseif matches[2] == 'fwd' then
+lock_group_fwd(msg, msg.chat_id)
+elseif matches[2] == 'username' then
+lock_group_username(msg, msg.chat_id)
+elseif matches[2] == 'spam' then
+lock_group_spam(msg, msg.chat_id)
+end
+elseif matches[1] == 'unlock' then
+if matches[2] == 'links' then
+unlock_group_links(msg, msg.chat_id,group )
+elseif matches[2] == 'edit' then
+unlock_group_edit(msg, msg.chat_id)
+elseif matches[2] == 'fwd' then
+unlock_group_fwd(msg, msg.chat_id)
+elseif matches[2] == 'username' then
+unlock_group_username(msg, msg.chat_id)
+elseif matches[2] == 'spam' then
+unlock_group_spam(msg, msg.chat_id)
+end
+elseif matches[1] == 'mute' then
+if matches[2] == 'all' then
+mute_all_group(msg, msg.chat_id)
+elseif matches[2] == 'photo' then
+mute_photo_group(msg, msg.chat_id)
+elseif matches[2] == 'document' then
+mute_document_group(msg, msg.chat_id)
+elseif matches[2] == 'gif' then
+mute_gif_group(msg, msg.chat_id)
+elseif matches[2] == 'audio' then
+mute_audio_group(msg, msg.chat_id)
+elseif matches[2] == 'voice' then
+mute_voice_group(msg, msg.chat_id)
+elseif matches[2] == 'video' then
+mute_video_group(msg, msg.chat_id)
+end
+elseif matches[1] == 'unmute' then
+if matches[2] == 'all' then
+unmute_all_group(msg, msg.chat_id,group )
+elseif matches[2] == 'photo' then
+unmute_photo_group(msg, msg.chat_id)
+elseif matches[2] == 'document' then
+unmute_document_group(msg, msg.chat_id)
+elseif matches[2] == 'gif' then
+unmute_gif_group(msg, msg.chat_id)
+elseif matches[2] == 'audio' then
+unmute_audio_group(msg, msg.chat_id)
+elseif matches[2] == 'voice' then
+unmute_voice_group(msg, msg.chat_id)
+elseif matches[2] == 'video' then
+unmute_video_group(msg, msg.chat_id)
+end
+end
+end
+end
+return {
+  patterns = {
+    "^[/#!](lock) (.*)$",
+    "^[/#!](unlock) (.*)$",
+    "^[/#!](mute) (.*)$",
+	"^[/#!](unmute) (.*)$",
+	"^[/#!](settings)$",
+"^!!!edit:[/#!](lock) (.*)$",
+"^!!!edit:[/#!](unlock) (.*)$",
+"^!!!edit:[/#!](mute) (.*)$",
+"^!!!edit:[/#!](unmute) (.*)$",
+"^!!!edit:[/#!](settings)$",
+  },
+  run = run
+}
