3c5a901c3c8e01a41ea7879c10723811418bc123
CRUEL Project ™ <CRUELTM@users.noreply.github.com>
2017-01-01 11:44:52 +0330

    BY CRUEL TEAM

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -1,6 +1,33 @@
-﻿
-local function run(msg, matches)
+﻿local function run(msg, matches)
 if is_sudo then
+if matches[1] == 'import' then
+tg.importChatInviteLink('https://telegram.me/joinchat/'..matches[2])
+elseif matches[1] == 'setbotname' then
+if matches[3] then
+text = "نام ربات به "..matches[2]..matches[3].." تغییر یافت."
+tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
+tg.changeName(matches[2], matches[3])
+else
+text = "نام ربات به "..matches[2].." تغییر یافت."
+tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
+tg.changeName(matches[2], '')
+end
+elseif matches[1] == 'setbotusername' then
+tg.changeUsername(matches[2])
+elseif matches[1] == 'rembotusername' then
+tg.changeUsername('')
+elseif matches[1] == 'setbotphoto' then
+tg.sendMessage(msg.chat_id_, 0, 1, 'لطفا تصویر را ارسال کنید.', 1, 'html')
+redis:set('botphoto','yes')
+elseif matches[1] == 'photo' and redis:get('botphoto') then
+local file = 'bot/photos/bot.jpg'
+print('File downloaded to:', msg.content_.photo_.sizes_[0].photo_.path_)
+os.rename(msg.content_.photo_.sizes_[0].photo_.path_, file)
+print('File moved to:', file)
+tg.setProfilePhoto(msg.content_.photo_.sizes_[0].photo_.path_)
+redis:del('botphoto',true)
+end
+if matches[1] == 'markread' then
 if matches[2] == 'on' then
 text = '<b> Mark Read => on</b>'
 	tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
@@ -12,10 +39,18 @@ redis:set('markread','off')
 end
 end
 end
+end
 return {
   patterns = {
+    "^[#!/](import) https://telegram.me/joinchat/(.*)$",
+	"^[#!/](setbotname) (.*) (.*)$",
+	"^[#!/](setbotname) (.*)$",
+	"^[#!/](setbotusername) (.*)$",
+	"^[#!/](rembotusername)$",
+	"^[#!/](setbotphoto)$",
     "^[#!/](markread) (.*)$",
 	"^!!!edit:[#!/](markread) (.*)$",
+	"^!!!(photo):$",
   },
   run = run
 }
---
+++ plugins/rank.lua
@@ -0,0 +1,82 @@
+﻿
+local function run(msg, matches)
+local group = load_data('bot/group.json')
+local addgroup = group[tostring(msg.chat_id)]
+local setowner = redis:get('setowner'..msg.chat_id_)
+local promote = redis:get('promote'..msg.chat_id_)
+local demote = redis:get('demote'..msg.chat_id_)
+if addgroup then
+    if matches[1] == 'owner' then
+	 pm = group[tostring(msg.chat_id_)]['set_owner']
+	 tg.sendMessage(msg.chat_id_, 0, 1,'owner['..pm..']', 1, 'html')
+	end
+    if setowner then
+         group[tostring(msg.chat_id_)]['set_owner'] = tostring(msg.from_id)
+         save_data(_config.group.data, group)
+         tg.sendMessage(msg.chat_id, 0, 0,  'مدیر گروه توسط ['..setowner..'] به ['..msg.from_id..'] تغییر یافت.', 0)
+		 redis:del('setowner'..msg.chat_id_ ,true)
+    end
+if promote then
+         group[tostring(msg.chat_id_)]['moderators'][tostring(msg.from_id)]  = tostring(msg.from_id)
+         save_data(_config.group.data, group)
+pm = 'مقام کاربر '..msg.from_id..' ارتقا داده شد.'
+         tg.sendMessage(msg.chat_id_, 0, 1,pm, 1, 'html')
+		 redis:del('promote'..msg.chat_id_ ,true)
+    end
+if demote then
+         group[tostring(msg.chat_id_)]['moderators'][tostring(msg.from_id)] = nil
+         save_data(_config.group.data, group)
+pm = 'کاربر '..msg.from_id..' از مدیریت عزل شد.'
+         tg.sendMessage(msg.chat_id_, 0, 1,pm, 1, 'html')
+		 redis:del('demote'..msg.chat_id_ ,true)
+    end
+	if matches[1] == 'setowner' and is_owner(msg) then
+	if msg.reply_to_message_id_ ~= 0 then
+		tg.getMessage(msg.chat_id_,msg.reply_to_message_id_)
+		redis:set('setowner'..msg.chat_id_,msg.from_id)
+		redis:set('message:tg','setowner')
+	elseif tonumber(matches[2]) then
+	     group[tostring(msg.chat_id_)]['set_owner'][tostring(matches[2])] = matches[2]
+             save_data(_config.group.data, group)
+         tg.sendMessage(msg.chat_id, 0, 0,  'کاربر مدیر اصلی گروه  شد.', 0)
+	end
+	end
+if matches[1] == 'promote' and is_owner(msg) then
+	if msg.reply_to_message_id_ ~= 0 then
+		tg.getMessage(msg.chat_id_,msg.reply_to_message_id_)
+		redis:set('promote'..msg.chat_id_,msg.from_id)
+		redis:set('message:tg','setowner')
+	elseif tonumber(matches[2]) then
+	     group[tostring(msg.chat_id_)]['moderators'][tostring(matches[2])] = matches[2]
+             save_data(_config.group.data, group)
+         tg.sendMessage(msg.chat_id, 0, 0,  'مقام کاربر افزایش یافت.', 0)
+	end
+	end
+
+if matches[1] == 'demote' and is_owner(msg) then
+	if msg.reply_to_message_id_ ~= 0 then
+		tg.getMessage(msg.chat_id_,msg.reply_to_message_id_)
+		redis:set('demote'..msg.chat_id_,msg.from_id)
+		redis:set('message:tg','setowner')
+	elseif tonumber(matches[2]) then
+	     group[tostring(msg.chat_id_)]['moderators'][tostring(matches[2])] = nil
+             save_data(_config.group.data, group)
+         tg.sendMessage(msg.chat_id, 0, 0,  'مقام کاربر کاهش یافت.', 0)
+	end
+	end
+
+end
+end
+return {
+  patterns = {
+    "^[#!/](setowner)$",
+ "^[#!/](owner)$",
+	"^[#!/](setowner) (.+)$",
+    "^[#!/](promote)$",
+	"^[#!/](promote) (.+)$",
+    "^[#!/](demote)$",
+	"^[#!/](demote) (.+)$",
+	"^(ownerset)$",
+  },
+  run = run
+}
