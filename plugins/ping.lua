0ae496eb8c69185344572ebf3c831fa4ad3cde81
CRUEL Project ™ <CRUELTM@users.noreply.github.com>
2017-01-04 01:48:03 -0800

    BY CRUEL TEAM

---
+++ plugins/ping.lua
@@ -0,0 +1,10 @@
+﻿local function run(msg, matches)
+tg.sendMessage(msg.chat_id, 0, 0,  "pong", 0)
+end
+return {
+  patterns = {
+    "^([Pp][Ii][Nn][Gg])$",
+	"^[!#/]([Pp][Ii][Nn][Gg])$",
+  },
+  run = run
+}
