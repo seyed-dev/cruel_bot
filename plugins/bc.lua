39a18c3d4dfd55ce88c445177e4b86ad2e1482fa
CRUEL Project ™ <CRUELTM@users.noreply.github.com>
2017-01-04 13:29:46 +0330

    BY CRUEL TEAM || add broadcast

--- plugins/bc.lua
+++ plugins/bc.lua
@@ -1,10 +1,19 @@
+
 local function run(msg, matches)
 	if matches[1] == 'bc' and is_sudo(msg) then
 		tg.sendMessage(matches[2], 0, 0,  matches[3], 0)
 	end
+	if matches[1] == 'broadcast' and is_sudo(msg) then
+		local group = load_data(_config.group.data)
+		local text = matches[2]
+			for k,v in pairs(group[tostring('groups')]) do
+				tg.sendMessage(v, 0, 0,  text, 0)
+			end
+	end
 end
 return {
   patterns = {
+    "^[#!/](broadcast) (.*)$",
     "^[#!/](bc) (%d+) (.*)$"
   },
   run = run
